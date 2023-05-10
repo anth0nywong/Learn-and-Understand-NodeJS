1348ae9c6f3de54f2d2d9b787f8bcbfbc4787170
anth0nywong <87212104+anth0nywong@users.noreply.github.com>
2023-05-10 04:18:53 -0400

    learning template engine

--- 7.0_Express/app.js
+++ 7.0_Express/app.js
@@ -1,27 +1,30 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
 let port = process.env.PORT || 3000;
 
-app.use('/assets', express.static(__dirname + '/public'));
+app.use("/assets", express.static(__dirname + "/public"));
 
-app.use('/', function(req, res, next){
-    console.log('Request Url: ' + req.url);
-    next();
-})
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res, next){
-    res.send('<html><head><link href=assets/style.css type=text/css rel="stylesheet"/></head><body><h1>Hello World!</h1></body></html>');
+app.use("/", function (req, res, next) {
+  console.log("Request Url: " + req.url);
+  next();
 });
 
-app.get('/api', function(req, res){
-    res.json({
-        firstname:'John', lastname:'Doe'
-    });
-})
+app.get("/", function (req, res, next) {
+  res.render("index");
+});
 
-app.get('/person/:id', function(req, res){
-    res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
-})
+app.get("/api", function (req, res) {
+  res.json({
+    firstname: "John",
+    lastname: "Doe",
+  });
+});
+
+app.get("/person/:id", function (req, res) {
+  res.render("person", { ID: req.params.id });
+});
 
 app.listen(port);
