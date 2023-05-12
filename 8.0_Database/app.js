c332b900e4bee21fd1705ec5674a291b6ec1c6dd
anth0nywong <87212104+anth0nywong@users.noreply.github.com>
2023-05-12 05:08:56 -0400

    mysql database on nodejs

---
+++ 8.0_Database/app.js
@@ -0,0 +1,36 @@
+let express = require("express");
+let app = express();
+let mysql = require("mysql");
+
+var apiController = require("./controllers/apiController");
+var htmlController = require("./controllers/htmlController");
+
+let port = process.env.PORT || 3000;
+
+app.use("/assets", express.static(__dirname + "/public"));
+
+app.set("view engine", "ejs");
+
+app.use("/", function (req, res, next) {
+  console.log("Request Url" + req.url);
+
+  let con = mysql.createConnection({
+    host: "localhost",
+    user: "user",
+    password: "ab92WWak",
+    database: "people",
+  });
+  con.query(
+    "SELECT people.ID, Firstname, Lastname, address.Address FROM people INNER JOIN peopleaddresses ON people.id = peopleaddresses.personID INNER JOIN address ON address.id = peopleaddresses.AddressID",
+    function (err, rows) {
+      if (err) throw err;
+      console.log(rows[0].Firstname + " " + rows[0].Lastname);
+    }
+  );
+  next();
+});
+
+apiController(app);
+htmlController(app);
+
+app.listen(port);
